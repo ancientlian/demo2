418a1e394dbb6e3dce81bdd6bbcd624228ff8c56
ancientlian <940262336@qq.com>
2022-04-27 16:32:29 +0800

    Initial commit

---
+++ src/components/About.vue
@@ -0,0 +1,102 @@
+<template>
+  <div>
+    <h1>About</h1>
+
+    <el-col :span="8">
+      <el-table
+        :data="tableData"
+        style="width: 100%">
+        <el-table-column
+          prop="date"
+          label="日期"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="姓名"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="地址">
+        </el-table-column>
+      </el-table>
+    </el-col>
+    <el-col style="margin-top: 20px" :span="16">
+      <div class="num">
+        <el-card v-for="item in countData" :key="item.name" :body-style="{display:'flex',padding: 0}">
+          <i class="icon" :class="`el-icon-${item.icon}`" :style="{background:item.color}"></i>
+          <div class="detail">
+            <p class="num">${{ item.value }}</p>
+            <p class="txt">{{ item.name }}</p>
+          </div>
+        </el-card>
+      </div>
+      <el-card style="height: 200px"></el-card>
+      <div class="graph">
+        <el-card style="height: 260px"></el-card>
+        <el-card style="height: 260px"></el-card>
+
+      </div>
+
+
+    </el-col>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "About",
+  data() {
+    return {
+      value: new Date(),
+      tableData: [{
+        date: '2016-05-02',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1518 弄'
+      }, {
+        date: '2016-05-04',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1517 弄'
+      }, {
+        date: '2016-05-01',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1519 弄'
+      }, {
+        date: '2016-05-03',
+        name: '王小虎',
+        address: '上海市普陀区金沙江路 1516 弄'
+      }],
+      countData: [
+        {
+          name: "今日支付订单",
+          value: 1234,
+          icon: "success",
+          color: "#2ec7c9"
+        }, {
+          name: "今日收藏订单",
+          value: 1234,
+          icon: "star-on",
+          color: "#ffb980"
+        },
+
+      ]
+    }
+  },
+  mounted() {
+    this.$http.get('/user?ID=12345')
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/config/index.js
@@ -0,0 +1,7 @@
+export default {
+  baseURL:{
+    dev:'/api/',
+    pro:'',
+
+  }
+}
